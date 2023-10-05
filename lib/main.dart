df15d7721e37a3783edd44743506b164c01c7290
Pouya Sadeghzadeh <122441650+MrPouyaSaad@users.noreply.github.com>
2023-10-05 23:45:31 +0330

    Make BottomNavigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -21,18 +22,45 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
 
   @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int index = 2;
+  final items = const [
+    Icon(Icons.home, size: 30),
+    Icon(Icons.search, size: 30),
+    Icon(Icons.favorite_sharp, size: 30),
+    Icon(Icons.settings, size: 30),
+    Icon(Icons.person, size: 30),
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBody: true,
       backgroundColor: Colors.blue,
       appBar: AppBar(
         backgroundColor: Colors.blue,
         title: const Text('Curved Navigation Bar'),
         centerTitle: true,
         elevation: 0,
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        animationCurve: Curves.easeInOut,
+        animationDuration: const Duration(milliseconds: 300),
+        index: index,
+        height: 60,
+        items: items,
+        onTap: (value) {
+          setState(() {
+            index = value;
+          });
+        },
       ),
     );
   }
